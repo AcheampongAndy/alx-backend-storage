75caea204c55ea206f54b1217d3455aa5df33cae
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-17 15:13:46 +0000

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Write a SQL script that creates a table users following these requirements:
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
-SELECT origin, SUM(nb_fans) AS fans
+SELECT origin AS origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY fans DESC;
+ORDER BY nb_fans DESC;
