f267c422a9b3d016efea6b88f642d347f8d18fd8
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-22 18:26:22 +0000

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -27,11 +27,3 @@ if __name__ == "__main__":
             if key['method'] == 'GET' and 'path' in key and key['path'] == '/status':
                 count += 1
 
-print("{0} logs".format(the_collection.count_documents({})))
-print("Methods:")
-print("\tmethod GET: {0}".format(methods['GET']))
-print("\tmethod POST: {0}".format(methods['POST']))
-print("\tmethod PUT: {0}".format(methods['PUT']))
-print("\tmethod PATCH: {0}".format(methods['PATCH']))
-print("\tmethod DELETE: {0}".format(methods['DELETE']))
-print("{0} status check".format(count))
