8360ba37b61b29ffe502b26e7689a3f3aeabe8bd
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-22 18:19:23 +0000

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" update all topics of doc """
+
+from typing import List
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name: str, topics: List[str]):
+    """ Updates all topics of school doc based on school name """
+
+    new_collection = mongo_collection.update_one({"name": name},{"$set":{"topics": topics}})
+
+    return new_collection
