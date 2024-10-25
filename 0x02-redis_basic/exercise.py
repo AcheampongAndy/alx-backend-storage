47689c24a9485d6b30f997903ba476c37a423a02
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-25 13:37:56 +0000

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Cache module for basic Redis operations.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 class Cache:
     """
@@ -33,3 +33,53 @@ class Cache:
         key = str(uuid.uuid4())
         self._radis.set(key, data)
         return key
+
+    def get(self, key: str, 
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves data from Redis and applies an optional conversion function.
+        
+        Args:
+            key (str): The key to retrieve the data from Redis.
+            fn (Optional[Callable]): A function to convert the data into the desired format.
+        
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data, optionally converted.
+        """
+        value = self._radis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves a string from Redis, decoding from bytes if necessary.
+
+        Args:
+            key (str): The key to retrieve the data from Redis.
+
+        Returns:
+            The data converted to a string
+        """
+        value = self._radis.get(key)
+        value = value.decode('utf-8')
+        return value
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves an integer from Redis, converting from bytes if necessary.
+
+        Args:
+            key (str): The key to retrieve the data from Redis.
+
+        Returns:
+            The data converted to an integer
+        """
+        value = self._radis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+
+        return value
+
